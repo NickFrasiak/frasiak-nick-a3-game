1fb9868d1d0297c13ae16863b24ab89bd3bdc3db
Nicholas Frasiak <000959762@mohawkcollege.ca>
2024-10-31 12:50:29 -0400

    add window setup add test pipe add colors

--- frasiak-nick-a3-flappybirdclone/Game.cs
+++ frasiak-nick-a3-flappybirdclone/Game.cs
@@ -10,15 +10,16 @@ namespace Game10003
     /// </summary>
     public class Game
     {
-        // Place your variables here:
-
+        Color skyColor = new Color(0x42, 0xbf, 0xe8);
+        Color pipeColor = new Color(0xf7, 0x76, 0x22);
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetTitle("Flappy ");
+            Window.SetSize(800, 600);
         }
 
         /// <summary>
@@ -26,7 +27,14 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(skyColor);
 
+            //Draw pipes
+            Draw.FillColor = pipeColor;
+            //bottom pipe
+            Draw.Rectangle(300, 350, 50, 350);
+            //top pipe
+            Draw.Rectangle(300, 0, 50, 250);
         }
     }
 }
